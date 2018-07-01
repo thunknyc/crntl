f9ec9f37dfd9ede4478fc838a2b6b23017815f2d
Edwin Watkeys <edw@poseur.com>
2018-07-01 08:12:39 -0400

    Clean up whitespace

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ void pad(FILE *out, int depth) {
     fprintf(out, LABEL " value: ");		\
     fputws(v->content.token.wcs, out);		\
     fputs("\n", out);				\
-  }  
+  }
 
 #define DUMPSEQUENCE(LABEL)						\
   {									\
@@ -67,7 +67,7 @@ void pad(FILE *out, int depth) {
       dumpval1(out, &elem->value.key_entry.k, depth + 2);		\
       dumpval1(out, &elem->value.key_entry.v, depth + 2);		\
     }									\
- }									
+  }
 
 #define DUMPTAGGED()							\
   {									\
@@ -80,7 +80,7 @@ void pad(FILE *out, int depth) {
 void dumpval1(FILE *out, struct ParserValue *v, int depth) {
   pad(out, depth);
   switch (v->type) {
-    
+
   case PRIMITIVE_VALUE:
     switch (v->content.token.type) {
     case INTVAL: DUMPPRIMITIVE("Integer"); break;
@@ -89,70 +89,56 @@ void dumpval1(FILE *out, struct ParserValue *v, int depth) {
     case KEYWORDVAL: DUMPPRIMITIVE("Keyword"); break;
     case STRINGVAL: DUMPPRIMITIVE("String"); break;
     case CHARVAL: DUMPPRIMITIVE("Character"); break;
-      
-    default: DUMPPRIMITIVE("Unknown"); break;	
+
+    default: DUMPPRIMITIVE("Unknown"); break;
     }
     break;
-    
+
   case LIST_VALUE: DUMPSEQUENCE("List"); break;
   case SET_VALUE: DUMPSEQUENCE("Set"); break;
   case FUNC_VALUE: DUMPSEQUENCE("Function"); break;
   case VECT_VALUE: DUMPSEQUENCE("Vect"); break;
-    
-  case DICT_VALUE: DUMPDICT(); break;      
+
+  case DICT_VALUE: DUMPDICT(); break;
 
   case TAGGED_VALUE: DUMPTAGGED(); break;
-    
-  case DEREF_VALUE:
-    DUMPBOX("Deref");
-    break;
-  case QUASIQUOTE_VALUE:
-    DUMPBOX("Quasiquote");
-    break;
-  case UNQUOTE_VALUE:
-    DUMPBOX("Unquote");
-    break;
-  case UNQUOTESPLICE_VALUE:
-    DUMPBOX("Unquote splice");
-    break;
-  case META_VALUE:
-    DUMPBOX("Meta");
-    break;
-  case VARQUOTE_VALUE:
-    DUMPBOX("Varquote");
-    break;
-  case QUOTE_VALUE: 
-    DUMPBOX("Quote");
-    break;
-    
+
+  case DEREF_VALUE: DUMPBOX("Deref"); break;
+  case QUASIQUOTE_VALUE: DUMPBOX("Quasiquote"); break;
+  case UNQUOTE_VALUE: DUMPBOX("Unquote"); break;
+  case UNQUOTESPLICE_VALUE: DUMPBOX("Unquote splice"); break;
+  case META_VALUE: DUMPBOX("Meta"); break;
+  case VARQUOTE_VALUE: DUMPBOX("Varquote"); break;
+  case QUOTE_VALUE: DUMPBOX("Quote"); break;
+
   case ERROR_VALUE:
     fprintf(out, "Error: %s\n", v->content.error_string);
     break;
-    
+
   case END_VALUE:
     fprintf(out, "End of input\n");
-      break;
+    break;
   }
 }
 
 void dumpval(FILE *out, struct ParserValue *v) {
   dumpval1(out, v, 0);
 }
-  
+
 int main(int argc, char **argv) {
   setlocale(LC_ALL, "en_US.UTF-8");
   struct TokenizerState ts;
   struct ParserValue value;
   int done = 0;
   int exit_status = 1;
-  
+
   crntl_state_init(&ts);
 
   while(!done) {
     crntl_read(stdin, &value, &ts);
 
     dumpval(stderr, &value);
-    
+
     switch (value.type) {
 
     case ERROR_VALUE:
